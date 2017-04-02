b45cd2de7582254cf5a55b7eb5a8dcd7157188f6
MandySingh3 <mandeeps139@gmail.com>
2017-04-03 04:53:25 +0530

    Small Mistake Big Error

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_CONFIG := addison_potter
+TARGET_KERNEL_CONFIG := potter_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
 
 # Audio

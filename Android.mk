553832af8330d60b864cfd72f99438c9f7db1235
Vachounet <vachounet@live.fr>
2017-04-18 13:28:06 +0200

    potter: symlink widevine firmware in vendor
    E/QSEECOMAPI(699): Error::Cannot open the file /vendor/firmware/widevine.mdt errno = 2
    E/QSEECOMAPI(699): Error::Loading image failed with ret = -1

--- Android.mk
+++ Android.mk
@@ -89,7 +89,7 @@ FIRMWARE_WIDEVINE_IMAGES := \
     widevine.b00 widevine.b01 widevine.b02 widevine.b03 \
     widevine.b04 widevine.b05 widevine.b06 widevine.mdt
 
-FIRMWARE_WIDEVINE_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_WIDEVINE_IMAGES)))
+FIRMWARE_WIDEVINE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_WIDEVINE_IMAGES)))
 $(FIRMWARE_WIDEVINE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Widevine Firmware link: $@"
 	@mkdir -p $(dir $@)

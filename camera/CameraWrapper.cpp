6afbd10ed4623cf5276bff483c6a100e6d1a013e
Vachounet <vachounet@live.fr>
2017-05-19 15:21:52 +0200

    potter: disable zsl in Snap only
    Change-Id: I0ca5e3f519775b8f3237e65aecff0f0e093e96fa

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -140,6 +140,8 @@ static char *camera_fixup_setparams(int id, const char *settings)
     params.dump();
 #endif
 
+    params.set("zsl", "on");
+
 #if !LOG_NDEBUG
     ALOGV("%s: fixed parameters:", __FUNCTION__);
     params.dump();

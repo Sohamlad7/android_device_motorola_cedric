7b12cd365c0d379c946c3cebcf68413af3ae18fa
Vachounet <vachounet@live.fr>
2017-05-23 15:52:03 +0200

    potter: set some flags for camera wrapper
     * Enable Vision FW
     * Enable PDAF Control Mode
     * Set AD ROI (seems enable on stock)
     * Set AE to frame-average
     * Let the HAL be more verbose for AE, AF and WB

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -119,6 +119,16 @@ static char *camera_fixup_getparams(int id, const char *settings)
     params.set("longshot-supported", "false");
     params.set("video-stabilization-supported", "false");
 
+    params.set("zsl", "on");
+    params.set("visionfw", "on");
+    params.set("pdaf-control-mode", "on");
+    params.set("af-roi-enabled", "on");
+    #params.set("auto-exposure", "frame-average");
+    #params.set("dbg-log-aec", "frame-average");
+    #params.set("dbg-log-awb", "frame-average");
+    #params.set("dbg-log-af", "on");
+
+
 #if !LOG_NDEBUG
     ALOGV("%s: fixed parameters:", __FUNCTION__);
     params.dump();
@@ -139,8 +149,6 @@ static char *camera_fixup_setparams(int id, const char *settings)
     ALOGV("%s: original parameters:", __FUNCTION__);
     params.dump();
 #endif
-
-    params.set("zsl", "on");
 
 #if !LOG_NDEBUG
     ALOGV("%s: fixed parameters:", __FUNCTION__);

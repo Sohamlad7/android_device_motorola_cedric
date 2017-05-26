ea9484ccb7d33cd08c75c712f0b5baf62cd73059
wzedlare <vedatak01@gmail.com>
2017-05-26 17:13:28 +0000

    cedric: Update cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,7 @@ TARGET_OTA_ASSERT_DEVICE := cedric,cedric_retail
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237
-BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=350M
+BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=350M androidboot.verifiedbootstate=green
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048

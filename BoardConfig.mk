9cea0ee87ab30bf04d0b9ca17aa7da757d7f343b
wzedlare <vedatak01@gmail.com>
2017-05-26 17:05:23 +0000

    cedric: Disable multi sensors hal
    We use prebuilt sensors hal

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,9 +144,6 @@ TARGET_USERIMAGES_USE_F2FS := true
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
-# Sensor
-USE_SENSOR_MULTI_HAL := true
-
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_WLAN_DEVICE := qcwcn

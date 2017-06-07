f7ad96d37b709384d57f60c49d28ec92d82d94eb
Alberto97 <albertop2197@gmail.com>
2017-06-07 09:53:30 +0200

    cedric: provide ASensor_getHandle for libmdmcutback

--- device.mk
+++ device.mk
@@ -249,7 +249,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2 \
-    rmnetcli
+    rmnetcli \
+    libshim_ril
 
 # Sensors
 PRODUCT_COPY_FILES += \
--- libshims/Android.mk
+++ libshims/Android.mk
@@ -39,3 +39,11 @@ LOCAL_SHARED_LIBRARIES := libstagefright_foundation
 LOCAL_MODULE := libshim_ims
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+# RIL
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := sensor.cpp
+LOCAL_SHARED_LIBRARIES := libgui
+LOCAL_MODULE := libshim_ril
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)

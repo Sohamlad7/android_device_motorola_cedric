8f1b83fd6c32791e79959bf608eaa166781604e4
Vachounet <vachounet@live.fr>
2017-06-21 10:43:28 +0200

    cedric: fix init_wifi service

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -35,6 +35,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := wlan_carrier_bin.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/wlan_carrier_bin.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.bt.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC

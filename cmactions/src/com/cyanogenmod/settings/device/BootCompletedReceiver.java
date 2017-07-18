e9bdc1ec3f5a9751706ade6dd6ffaa7c7203efad
Vachounet <vachounet@live.fr>
2017-07-18 10:04:18 +0200

    CMActions: show navbar at first boot
    Change-Id: I02bb7126c833bb6e6a88bca2b02549513c881763

--- cmactions/src/com/cyanogenmod/settings/device/BootCompletedReceiver.java
+++ cmactions/src/com/cyanogenmod/settings/device/BootCompletedReceiver.java
@@ -20,14 +20,19 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.util.Log;
+
+import cyanogenmod.providers.CMSettings;
 
 import com.cyanogenmod.settings.device.ServiceWrapper.LocalBinder;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     static final String TAG = "CMActions";
+    final String NAVBAR_SHOWN = "navbar_shown";
     private ServiceWrapper mServiceWrapper;
 
     @Override
@@ -39,7 +44,19 @@ public class BootCompletedReceiver extends BroadcastReceiver {
              Constants.writePreference(context, pref);
         }
 
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+
+        if (!preferences.getBoolean(NAVBAR_SHOWN, false)) {
+            enableNaviBar(true, context);
+            preferences.edit().putBoolean(NAVBAR_SHOWN, true).commit(); 
+        }
+
         context.startService(new Intent(context, ServiceWrapper.class));
+    }
+
+    protected static void enableNaviBar(boolean enable, Context context) {
+        CMSettings.Global.putInt(context.getContentResolver(),
+                CMSettings.Global.DEV_FORCE_SHOW_NAVBAR, enable ? 1 : 0);
     }
 
     private ServiceConnection serviceConnection = new ServiceConnection() {

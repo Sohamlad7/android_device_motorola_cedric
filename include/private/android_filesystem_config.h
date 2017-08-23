93e3e566e068c04fd600aab9a68d01363895071f
Vachounet <vachounet@live.fr>
2017-08-23 11:53:46 +0200

    cedric: define AID_USER_OFFSET
    Change-Id: I7bd776664fd288b2a02835a894099d1933a2a2b1

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -160,7 +160,8 @@
 #define AID_ISOLATED_START 99000 /* start of uids for fully isolated sandboxed processes */
 #define AID_ISOLATED_END   99999 /* end of uids for fully isolated sandboxed processes */
 
-#define AID_USER        100000  /* offset for uid ranges for each user */
+#define AID_USER 100000        /* TODO: switch users over to AID_USER_OFFSET */
+#define AID_USER_OFFSET 100000 /* offset for uid ranges for each user */
 
 #define AID_SHARED_GID_START 50000 /* start of gids for apps in each user to share */
 #define AID_SHARED_GID_END   59999 /* start of gids for apps in each user to share */

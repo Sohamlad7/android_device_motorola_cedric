36600a693538a4b183c34006c68fac6085a2d31c
Vachounet <vachounet@live.fr>
2017-09-04 13:25:14 +0200

    cedric: update bluetooth config
    Change-Id: Idcadc9aad0ff07ef8fd68bb7de40573f159d5610

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,7 +21,9 @@
 #include <string.h>
 
 #define BTM_DEF_LOCAL_NAME "Moto G5"
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BLUETOOTH_QTI_SW TRUE
-#define BLE_VND_INCLUDED   TRUE
+#define MAX_ACL_CONNECTIONS   16
+#define MAX_L2CAP_CHANNELS    16
+#define BLE_VND_INCLUDED TRUE
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
 #endif

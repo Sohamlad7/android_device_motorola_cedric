a7dcfd6f484956c36da64dd72112a7f7bdb39369
Vachounet <vachounet@live.fr>
2017-09-08 14:29:37 +0200

    cedric: sensors: copy proper hals.conf
    Change-Id: Ida62267f5e7cd11d0f2018f3a88de43803636e4d

--- device.mk
+++ device.mk
@@ -298,7 +298,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensor Configurations
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:system/vendor/etc/sensors/_hals.conf \
+    $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
 
 PRODUCT_PACKAGES += \

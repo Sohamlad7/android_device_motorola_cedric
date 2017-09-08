87773dd845aefa5c4891466655a4300043dd7c02
LuK1337 <priv.luk@gmail.com>
2017-09-08 13:46:20 +0200

    cedric: add basic 2.0 -> 2.1 fingerprint HAL wrapper
    Change-Id: I27963bfb1402aba10e4ceba669520b630d075abf

--- device.mk
+++ device.mk
@@ -161,7 +161,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
+    fingerprint.msm8937
 
 # Fingerprint IDC
 PRODUCT_COPY_FILES += \

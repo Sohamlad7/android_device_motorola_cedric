6a0ec9a65f40e3f7da411bd44449ed6df5ad4996
Vachounet <vachounet@live.fr>
2017-09-08 13:44:46 +0200

    cedric: add back CMActions
    Change-Id: Ib3ab414d6684c2ced1a3c998e3d3933f3334b2d0

---
+++ cmactions/src/com/cyanogenmod/settings/device/FlipToMute.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.device;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.util.Log;
+
+public class FlipToMute implements UpdatedStateNotifier {
+    private static final String TAG = "CMActions-FlipToMute";
+
+    private final NotificationManager mNotificationManager;
+    private final CMActionsSettings mCMActionsSettings;
+    private final SensorHelper mSensorHelper;
+    private final Sensor mFlatDown;
+    private final Sensor mStow;
+
+    private boolean mIsEnabled;
+    private boolean mIsFlatDown;
+    private boolean mIsStowed;
+    private int mFilter;
+    private Context mContext;
+    private Receiver mReceiver;
+
+    public FlipToMute(CMActionsSettings cmActionsSettings, Context context,
+                SensorHelper sensorHelper) {
+        mCMActionsSettings = cmActionsSettings;
+        mContext = context;
+        mSensorHelper = sensorHelper;
+        mFlatDown = sensorHelper.getFlatDownSensor();
+        mStow = sensorHelper.getStowSensor();
+        mNotificationManager =
+            (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mFilter = mNotificationManager.getCurrentInterruptionFilter();
+        mReceiver = new Receiver();
+    }
+
+    @Override
+    public void updateState() {
+        if (mCMActionsSettings.isFlipToMuteEnabled() && !mIsEnabled) {
+            Log.d(TAG, "Enabling");
+            mSensorHelper.registerListener(mFlatDown, mFlatDownListener);
+            mSensorHelper.registerListener(mStow, mStowListener);
+            mContext.registerReceiver(mReceiver,
+                new IntentFilter(NotificationManager.ACTION_INTERRUPTION_FILTER_CHANGED));
+            mIsEnabled = true;
+        } else if (!mCMActionsSettings.isFlipToMuteEnabled() && mIsEnabled) {
+            Log.d(TAG, "Disabling");
+            mSensorHelper.unregisterListener(mFlatDownListener);
+            mSensorHelper.unregisterListener(mStowListener);
+            mContext.unregisterReceiver(mReceiver);
+            mIsEnabled = false;
+        }
+    }
+
+    private SensorEventListener mFlatDownListener = new SensorEventListener() {
+        @Override
+        public synchronized void onSensorChanged(SensorEvent event) {
+            mIsFlatDown = (event.values[0] != 0);
+            sensorChange();
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor mSensor, int accuracy) {
+        }
+    };
+
+    private SensorEventListener mStowListener = new SensorEventListener() {
+        @Override
+        public synchronized void onSensorChanged(SensorEvent event) {
+            mIsStowed = (event.values[0] != 0);
+            sensorChange();
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor mSensor, int accuracy) {
+        }
+    };
+
+    private void sensorChange() {
+
+        Log.d(TAG, "event: " + mIsFlatDown + " mIsStowed=" + mIsStowed);
+
+        if (mIsFlatDown && mIsStowed) {
+            mNotificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_PRIORITY);
+            Log.d(TAG, "Interrupt filter: Allow priority");
+        } else if (!mIsFlatDown) {
+            mNotificationManager.setInterruptionFilter(mFilter);
+            Log.d(TAG, "Interrupt filter: Restore");
+        }
+    }
+
+    public class Receiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (!mIsFlatDown && !mIsStowed) {
+                mFilter = mNotificationManager.getCurrentInterruptionFilter();
+                Log.d(TAG, "Interrupt filter: Backup");
+            }
+        }
+    }
+}

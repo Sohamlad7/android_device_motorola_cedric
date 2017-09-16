212c2089fdbc27fff3146e68091e8c300b489dc3
Vachounet <vachounet@live.fr>
2017-09-16 13:46:27 +0200

    cedric: build libtinycompress
    Change-Id: I5d4c74cd4f8ccdac796e5d6d0312b6dd9a3fe948

--- device.mk
+++ device.mk
@@ -61,7 +61,8 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     audio.r_submix.default \
     libaudio-resampler \
-    libshim_adsp
+    libshim_adsp \
+    libtinycompress
 
 PRODUCT_PACKAGES += \
     libqcomvisualizer \

Alberto97/android_device_motorola_addison

Switch to OSS GPS
Change-Id: I27111e9a27b2cd48e4c40276da553a7a146cde70

device.mk
@@ -148,8 +148,9 @@ PRODUCT_PACKAGES += \
     fs_config_files
 
 # GPS
-#PRODUCT_PACKAGES += \
-#    gps.msm8916
+PRODUCT_PACKAGES += \
+    libgnsspps \
+    gps.msm8953
 
 # IMS
 PRODUCT_PACKAGES += \

Sohamlad7/android_device_motorola_cedric

device.mk
@@ -184,6 +184,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
+# GPS
+PRODUCT_PACKAGES += \
+    libgnsspps \
+    gps.msm8937
+
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl
PixelExperience-Devices/device_motorola_sanders

gps: Add get_process_name shim
Change-Id: Iff9c1e33b9cd0343f5c4ea7f9a4c84c562c4142b

device.mk
@@ -151,7 +151,8 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     libgnsspps \
-    gps.msm8953
+    gps.msm8953 \
+    libshims_get_process_name
 
 # IMS
 PRODUCT_PACKAGES += \

Sohamlad7/android_device_motorola_cedric

device.mk
@@ -187,7 +187,8 @@ PRODUCT_COPY_FILES += \
 # GPS
 PRODUCT_PACKAGES += \
     libgnsspps \
-    gps.msm8937
+    gps.msm8937 \
+    libshims_get_process_name
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
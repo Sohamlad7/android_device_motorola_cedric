8854f2110d0813f1187f95ae63d5fba2f89070e1
Vachounet <vachounet@live.fr>
2017-10-02 14:48:34 +0200

    cedric: use lzma to compress recovery ramdisk
    Change-Id: I75a73e9a49360a95fd8700d65360a781302ae854
    error: +out/target/product/potter/recovery.img too large (17537024 > [17407104 - 270336])

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,6 +167,7 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+LZMA_RAMDISK_TARGETS := recovery
 
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk

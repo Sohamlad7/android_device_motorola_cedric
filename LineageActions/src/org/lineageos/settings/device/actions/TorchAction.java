76b481412168641eb051258f7faa42d238094b5a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-03 18:09:57 -0200

    cedric: LineageActions: Register torch callback on chop-chop action

--- LineageActions/src/org/lineageos/settings/device/actions/TorchAction.java
+++ LineageActions/src/org/lineageos/settings/device/actions/TorchAction.java
@@ -38,6 +38,7 @@ public class TorchAction implements SensorAction {
 
     public TorchAction(Context mContext) {
         mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+        mCameraManager.registerTorchCallback(new MyTorchCallback(), null);
         mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
         try {
             for (final String cameraId : mCameraManager.getCameraIdList()) {

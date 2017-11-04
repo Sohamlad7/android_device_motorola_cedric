91b2eb79c687946dc269a30f7d4624111fd06ab2
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-04 13:48:44 -0200

    cedric: Build hidl DRM service and add to the manifest

--- device.mk
+++ device.mk
@@ -220,7 +220,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-impl
 
 # Media
 PRODUCT_PACKAGES += \

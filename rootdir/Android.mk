fbfdfaf9f3cdff121facf87bc739b1de3e501479
Wzedlare <vedatak01@gmail.com>
2017-11-09 14:56:21 +0000

    cedric: Set governor setups using the post-boot script
    Sync governor setups with the stock rom.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -57,6 +57,13 @@ LOCAL_SRC_FILES    := etc/init.qcom.fm.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.post_boot.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.post_boot.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.ril.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC

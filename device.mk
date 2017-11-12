fd87203d3c9f88457e4cc633b1490dcda7200a56
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-12 01:05:28 -0200

    cedric: Remove soundtrigger and DS2_DOLBY_DAP
    cedric doesn't have on stock rom
    
    11-12 00:57:45.084   791   791 E soundtrigger: audio_extn_sound_trigger_init: DLOPEN failed for /vendor/lib/hw/sound_trigger.primary.msm8953.so. error = dlopen failed: library "/vendor/lib/hw/sound_trigger.primary.msm8953.so" not found
    
    11-12 00:57:45.075   791   791 E audio_hw_dolby: audio_extn_dap_hal_init: DLOPEN failed for vendor/lib/libhwdaphal.so error dlopen failed: library "vendor/lib/libhwdaphal.so" not found

--- device.mk
+++ device.mk
@@ -75,8 +75,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.broadcastradio@1.0-impl
 
 # Audio Configurations
 PRODUCT_COPY_FILES +=  \

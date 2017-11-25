2d13880a25691ca494e65ae7e1960edddfd3cd6d
Vachounet <vachounet@live.fr>
2017-11-25 10:38:27 +0100

    cedric: revert some binderized services

--- device.mk
+++ device.mk
@@ -122,11 +122,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    android.hardware.configstore@1.0-service \
     hwcomposer.msm8937 \
     memtrack.msm8937 \
     libgenlock \
@@ -136,8 +133,7 @@ PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl \
-    android.hardware.renderscript@1.0-service
+    android.hardware.renderscript@1.0-impl
 
 # DRM
 PRODUCT_PACKAGES += \

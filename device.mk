d261d60ed5c30638409055587904c662c610f823
Bindu Gokul <bgokul@codeaurora.org>
2017-11-28 16:03:58 +0530

    cedric: remove android.hardware.usb; no usbpd support
    Change-Id: Icb46be06afd4f23252439e481e36741cde346612

--- device.mk
+++ device.mk
@@ -355,10 +355,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
-# USB HAL
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \

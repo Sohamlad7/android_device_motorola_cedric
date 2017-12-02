a91e4c9f9bf9f5ba356f66c6c31c7b1c09ebd4ae
Jobin Johnson <jobinrjohnson@gmail.com>
2017-12-02 07:06:53 +0000

    cedric: clean up removed unwanted makefiles added licence
    Signed-off-by: Jobin Johnson <jobinrjohnson@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2015 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 LOCAL_PATH := $(call my-dir)
 

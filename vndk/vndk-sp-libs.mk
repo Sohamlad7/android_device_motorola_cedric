bca6b6971d7a46ae19fe1304a1a7097527e507d0
Logan Chien <loganchien@google.com>
2017-12-12 11:34:42 +0800

    cedric: android.hardware.graphics.allocator@2.0 is no longer VNDK-SP
    The interface lib has been in VNDK-SP because
    android.hardware.graphics.mapper@1.0 was using it. However, since the
    dependency has gone [1], there is no need keep it in VNDK-SP. The
    VNDK-SP set should be kept as small as possible because libs in VNDK-SP
    are subject to double-loading.
    
    [1] https://android.googlesource.com/platform/hardware/interfaces/+/79d13ff0f5cd3c4242445baf5b2f01a52f0ed06f
    
    Bug: 69480083
    Test: /system/lib64/vndk-sp/android.hardware.graphics.allocator@2.0.so
    disappears.
    Test: sailfish boots
    
    Change-Id: I66ddfc91e6178def39671bd5f90330b4601cfd89
    Merged-In: I66ddfc91e6178def39671bd5f90330b4601cfd89
    (cherry picked from commit 80b077ed612e2e9fa03722545f048484e974df70)
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -1,5 +1,4 @@
 VNDK_SP_LIBRARIES := \
-    android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.mapper@2.0 \
     android.hardware.graphics.mapper@2.1 \
     android.hardware.graphics.common@1.0 \

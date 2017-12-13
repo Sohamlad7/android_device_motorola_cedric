ce299974ef3c1f1bd9237910e5b2d1072e27d8a0
Vachounet <vachounet@live.fr>
2017-12-13 11:48:25 +0100

    cedric: switch back to core_64_bit.mk
    * Use zygote64_32 instead of zygote32_64

--- full_cedric.mk
+++ full_cedric.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, device/motorola/cedric/core_32_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from cedric device

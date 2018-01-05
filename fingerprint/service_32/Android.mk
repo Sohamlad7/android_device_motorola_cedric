5bc1c787b20fb4276707aed7a0d0109c1dda44eb
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-05 13:31:25 -0200

    cedric: Build a 32 bit version of rild and fp service
    For now we're using 32 bits from stock, for both services. So use it until 64 bits blobs are ready
    
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>

---
+++ fingerprint/service_32/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.1-service_32
+LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.1-service_32.rc
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_32_BIT_ONLY := true
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SRC_FILES := \
+    ../../../../../hardware/interfaces/biometrics/fingerprint/2.1/default/BiometricsFingerprint.cpp \
+    ../../../../../hardware/interfaces/biometrics/fingerprint/2.1/default/service.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    liblog \
+    libhidlbase \
+    libhidltransport \
+    libhardware \
+    libutils \
+    android.hardware.biometrics.fingerprint@2.1
+
+include $(BUILD_EXECUTABLE)

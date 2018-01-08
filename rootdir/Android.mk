837f5d56ea9127c7f52da1602f1689fdde1b2e2c
Vachounet <vachounet@live.fr>
2018-01-08 13:18:25 +0100

    cedric: remove init.qcom.ril.sh
    * Not used

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -57,14 +57,6 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.post_boot.sh
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.ril.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.ril.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
 # Init scripts
 
 include $(CLEAR_VARS)

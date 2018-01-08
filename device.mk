6de9af225bff6c711883f65efd4d5121eb5d604c
Vachounet <vachounet@live.fr>
2018-01-08 11:46:55 +0100

    cedric: use 64bit rild
    * Partialy revert a3efcae

--- device.mk
+++ device.mk
@@ -292,11 +292,7 @@ PRODUCT_PACKAGES += \
     libshim_ril \
     libprotobuf-cpp-full \
     libxml2 \
-    rild_socket \
-    rild_32
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rild/rild.rc:system/vendor/etc/init/rild.rc
+    rild_socket
 
 #RIL
 PRODUCT_PACKAGES += \

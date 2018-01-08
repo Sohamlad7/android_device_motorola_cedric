561e25f8af5dd666f47a4879fe9edf2f2530a0bb
Vachounet <vachounet@live.fr>
2018-01-08 09:55:39 +0100

    cedric: enable binderized bluetooth

--- device.mk
+++ device.mk
@@ -92,7 +92,8 @@ PRODUCT_COPY_FILES +=  \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \

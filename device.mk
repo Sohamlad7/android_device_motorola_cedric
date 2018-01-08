bb22762d20b4572553eb540063688ed6e31cf83c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-08 16:24:58 -0200

    cedric: sepolicy: Remove leftovers from few services

--- device.mk
+++ device.mk
@@ -264,9 +264,6 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    init.mmi.touch.sh \
-    init.qcom.ril.sh \
-    init.qcom.bt.sh \
     init.qcom.fm.sh \
     init.qcom.post_boot.sh \
     wlan_carrier_bin.sh
@@ -277,7 +274,6 @@ PRODUCT_PACKAGES += \
     init.mmi.laser.sh \
     init.mmi.rc \
     init.mmi.usb.rc \
-    init.oem.hw.sh \
     init.qcom.rc \
     ueventd.qcom.rc
 

aab41c7bea8ea23ab542f51321fa445cd7e1b4f3
Vachounet <vachounet@live.fr>
2018-01-16 17:38:52 +0100

    cedric: build lineage vibrator HAL

--- device.mk
+++ device.mk
@@ -335,8 +335,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.lineage
 
 PRODUCT_PACKAGES += \
     vndk-sp

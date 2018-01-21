3b84dc706ac5eb8efba4240e6a95680858bc82d0
Vachounet <vachounet@live.fr>
2018-01-21 22:13:39 +0100

    Revert "cedric: set permissive"
    Change-Id: I80889b8f38808e6c550ed7a142cbc05e9696cd67
    (reverted from commit b4cec1bf39251f6f2b17f0e2fd4cc122b46e1536)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,7 +66,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237
-BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=350M androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=350M
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_PAGESIZE := 2048

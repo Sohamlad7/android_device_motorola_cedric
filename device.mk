e9266ed681efbdc869fedcd12454eafe1bb5f6ec
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-22 20:11:17 -0200

    cedric: Nuke data-ipa-cfg-mgr

--- device.mk
+++ device.mk
@@ -195,11 +195,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
 
-# IPA Manager
-PRODUCT_PACKAGES += \
-    ipacm \
-    IPACM_cfg.xml
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/vendor/etc/sec_config

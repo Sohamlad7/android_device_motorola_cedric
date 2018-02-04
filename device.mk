b9319a0ac0a1a72c9b82df1b48a086605e1b2d37
Vachounet <vachounet@live.fr>
2018-02-04 17:18:14 +0100

    cedric: add health HIDL

--- device.mk
+++ device.mk
@@ -193,6 +193,7 @@ PRODUCT_PACKAGES += \
 
 # health
 PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-convert \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
 

f2b14a442bc21153cec9add8141cc044de96b304
Bruno Martins <bgcngm@gmail.com>
2018-04-01 10:58:29 +0100

    cedric: libinit: Also override certain vendor build properties

--- init/init_cedric.cpp
+++ init/init_cedric.cpp
@@ -75,13 +75,13 @@ void vendor_load_properties()
     if (platform != ANDROID_TARGET)
         return;
 
+    // sku
     std::string sku = android::base::GetProperty("ro.boot.hardware.sku", "");
+    property_override_dual("ro.product.model", "ro.vendor.product.model", sku.c_str());
 
     // fingerprint
     property_override("ro.build.description", "cedric-7.0/NPPS25.137-72-4/4:user/release-keys");
-    property_override("ro.build.fingerprint", "motorola/cedric/cedric:7.0/NPPS25.137-72-4/4:user/release-keys");
-
-    property_set("ro.product.model", sku.c_str());
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "motorola/cedric/cedric:7.0/NPPS25.137-72-4/4:user/release-keys");
 
     // rmt_storage
     std::string device = android::base::GetProperty("ro.boot.device", "");

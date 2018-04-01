69ec1088e54c219510fdad9cbe0e3c335ff84c0a
Bruno Martins <bgcngm@gmail.com>
2018-04-01 02:56:00 -0700

    cedric: init: Handle overriding system and vendor props
    * Up until now, vendor build properties were added into /system/build.prop
       when property split isn't enabled. However, Google realised that
       either /vendor/build.prop or /system/vendor/build.prop can always exist,
       and decided to clean up code and always install vendor build props
       into $(TARGET_OUT_VENDOR)/build.prop.
    
     * For this reason, the unified devices that used to override build props
       such as build fingerprint, will need to override the matching vendor
       build props.
    
    Change-Id: Iacdd8eb67543daff5a46b92dbaf17cd094ce462b

--- init/init_cedric.cpp
+++ init/init_cedric.cpp
@@ -48,6 +48,13 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void property_override_dual(char const system_prop[], char const vendor_prop[],
+    char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
 void num_sims() {
     std::string dualsim;
 

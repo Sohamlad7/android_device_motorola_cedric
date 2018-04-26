4e92dbd76ba7bf65d512b02d3b3c256008622a8d
Vachounet <vachounet@live.fr>
2018-04-26 11:20:42 +0200

    cedric: remove TARGET_USES_MEDIA_EXTENSION flag
    * It's now default enable for all QC devices : https://review.lineageos.org/#/c/213117/

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,9 +184,6 @@ BOARD_HARDWARE_CLASS += \
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/configs/compatibility_matrix.xml
 
-# Media
-# TARGET_USES_MEDIA_EXTENSIONS := true
-
 PRODUCT_BOOT_JARS += telephony-ext
 
 # Partitions

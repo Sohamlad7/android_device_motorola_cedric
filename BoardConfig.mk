69905d6f0508804a8a1f4ab5fe7d173da407eeef
Vachounet <vachounet@live.fr>
2018-05-16 13:52:51 +0200

    cedric: update shims path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -235,7 +235,7 @@ MOT_SENSOR_HUB_FEATURE_GR := true
 TARGET_LD_SHIM_LIBS := \
     /vendor/bin/adspd|libshim_adsp.so \
     /vendor/lib64/libmdmcutback.so|libqsap_shim.so \
-    /system/lib/libjustshoot.so|libjustshoot_shim.so
+    /vendor/lib/libjustshoot.so|libjustshoot_shim.so
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true

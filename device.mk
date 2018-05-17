c2e0f29a42674f47a3d5ce5f7d8979efa85c81cc
Himanshu Agrawal <hagraw@codeaurora.org>
2018-05-17 17:24:14 +0530

    cedric: Changing healthd version to 2.0
    Change-Id: I13ddaa133bb9edcca200d5fe7320cf5fa9f66f0f

--- device.mk
+++ device.mk
@@ -204,9 +204,8 @@ PRODUCT_PACKAGES += \
 
 # health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-convert \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # IRSC
 PRODUCT_COPY_FILES += \

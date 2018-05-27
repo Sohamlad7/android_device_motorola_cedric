c57dd23edbea13020b7666ff84362f90201fa415
Pedro Kalil <kalilgamer0@gmail.com>
2018-05-27 04:05:41 -0300

    cedric: update p-files.txt and p-files64.txt *based on the cedric vendor at the https://github.com/Sohamlad7/android_vendor_motorola_cedric/commit/d12e5e5389e1fd564c138796348c8f7ae2f1e010 state *split between architectures *sorted by name

--- extract-files.sh
+++ extract-files.sh
@@ -60,12 +60,14 @@ fi
 setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+extract "$MY_DIR"/proprietary-files64.txt "$SRC" "$SECTION"
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
     setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
     extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC" "$SECTION"
+    extract "$MY_DIR"/../$DEVICE/proprietary-files64.txt "$SRC" "$SECTION"
 fi
 
 "$MY_DIR"/setup-makefiles.sh

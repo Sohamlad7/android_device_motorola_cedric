7396fbc661e53c2a23d7c714891246492c632bca
Sohamlad7 <sohamlad7@gmail.com>
2018-05-29 12:36:38 +0530

    cedric: Move liblight and fp wrapper to vendor
    * vendor.prop: force nav bar to be enabled by default for some gsi
    
    Change-Id: I9e9cb2399ed4ac717ffb715dc740ab30501ce485

--- fingerprint/service_32/Android.mk
+++ fingerprint/service_32/Android.mk
@@ -6,6 +6,7 @@ LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.1-service_32.rc
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_VENDOR_MODULE := true
 LOCAL_SRC_FILES := \
     ../../../../../hardware/interfaces/biometrics/fingerprint/2.1/default/BiometricsFingerprint.cpp \
     ../../../../../hardware/interfaces/biometrics/fingerprint/2.1/default/service.cpp

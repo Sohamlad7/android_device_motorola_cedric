a8d3f8792b646f1b0cb0e8ae06ec5f9ef47e5fcf
Sohamlad7 <sohamlad7@gmail.com>
2018-06-02 22:23:24 +0530

    cedric: Enable hal3 and update camera configs
    * Remove the camerawrapper, as its not needed in hal3
    * Add potter vfwconfig but dont enable it
    * Rename the cedric camera config for it to work with the camerahal ported from potter by @KalilDev
    * Make the camera config for the secondary camera inline with potter in hopes to get the potter ported camerahal working

--- device.mk
+++ device.mk
@@ -109,17 +109,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbson \
     Camera2 \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
-    camera.msm8937
+    vendor.qti.hardware.camera.device@1.0_vendor
 
-# Camera Configurations
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/camera/msm8937_mot_camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/msm8937_mot_camera.xml \
+    $(LOCAL_PATH)/configs/camera/msm8937_mot_cedric_camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/msm8937_mot_cedric_camera.xml \
     $(LOCAL_PATH)/configs/camera/mot_ov5695_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mot_ov5695_chromatix.xml \
     $(LOCAL_PATH)/configs/camera/mot_imx258_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mot_imx258_chromatix.xml
+
+#PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/camera/vfwconfig.json:$(TARGET_COPY_OUT_VENDOR)/etc/camera/vfwconfig.json
 
 # LineageActions
 PRODUCT_PACKAGES += \

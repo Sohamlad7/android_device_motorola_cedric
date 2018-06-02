d45e4631b1b67ccddc1d34ae6e49023012109a78
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-06-02 19:32:31 +0200

    cedric: don't build camera service
    * Conflicts with Moto one

--- device.mk
+++ device.mk
@@ -111,7 +111,6 @@ PRODUCT_PACKAGES += \
     Camera2 \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
     camera.msm8937

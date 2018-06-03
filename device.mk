a1f7b7f238625a3771b394c9092c01ab99bcef4a
Vachounet <vachounet@live.fr>
2018-06-03 11:39:54 +0200

    cedric: build Snap
    * And enable api v2

--- device.mk
+++ device.mk
@@ -108,7 +108,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    Camera2 \
+    Snap \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \

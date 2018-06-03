8471475f29270302b233d07c93083436cfd884f7
Vachounet <vachounet@live.fr>
2018-06-03 16:59:00 +0200

    cedric: move LineageActions to vendor
    Change-Id: I3d29d90a0f1209330763738457954991f9880778

--- LineageActions/Android.mk
+++ LineageActions/Android.mk
@@ -7,7 +7,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := LineageActions
 LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PROPRIETARY_MODULE := true
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v14-preference \

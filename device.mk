e19e0ecea995e0534d04fcb0076bce69036ff506
Sohamlad7 <sohamlad7@gmail.com>
2018-07-02 12:24:27 +0530

    cedric: add some HALs
    Change-Id: Iadffea787d38e136e6e4e7ed589d542b3a34ee30

--- device.mk
+++ device.mk
@@ -125,15 +125,19 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     android.hardware.configstore@1.0-service \
     hwcomposer.msm8937 \
     memtrack.msm8937 \
     libgenlock \
     libtinyxml \
     libdisplayconfig \
-    libqdMetaData.system
+    libqdMetaData.system \
+    vendor.display.config@1.0 \
+    vendor.display.config@1.0_vendor
 
 PRODUCT_PACKAGES += android.hardware.media.omx
 

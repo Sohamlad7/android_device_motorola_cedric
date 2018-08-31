91d42b080bac1a14c72a963b9acc6fffdad17a57
Danny Baumann <dannybaumann@web.de>
2018-08-31 13:35:20 +0200

    cedric: Mark camera-related processes as API level 26.
    Our camera blobs trigger some checks for broken behavior (destroyed
    mutex usage, invalid pthread usage) present in newer API levels.
    
    Change-Id: I8705437e0a5c6d80b86748bff559ce1e8a7bf716

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,6 +129,8 @@ BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
 
 # Camera
+TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
+    /system/vendor/bin/mm-qcamera-daemon=26
 USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_QTI_CAMERA_32BIT_ONLY := true
 

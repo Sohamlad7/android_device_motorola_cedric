f5d14313742a1bd13de92f3f5c85a54a7828b998
therootlord <igor_cestari@hotmail.com>
2018-09-14 13:46:03 -0300

    cedric: don't build android.hardware.bluetooth@1.0-impl and service
    * We're using qcom's ones

--- device.mk
+++ device.mk
@@ -94,9 +94,7 @@ PRODUCT_COPY_FILES +=  \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    libbt-vendor \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
+    libbt-vendor
 
 # Camera
 PRODUCT_PACKAGES += \

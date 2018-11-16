27bbe07736965770ef931e85dc39ad00c18faac6
Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
2018-11-16 13:15:51 +0530

    cedric: Enabling USB enumeration in power-off charging mode
    sys.usb.configfs can't be set from .sh file as per new Android
    sepolicy rules. Hence set sys.usb.configfs in target rc file based on
    ro.boot.usbconfigfs property for USB to work properly in
    power-off charging mode.
    
    Change-Id: I075c1d33e4018099e3deca64e8b2737f1a0c1d4b
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,7 +68,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 androidboot.usbconfigfs=false
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=350M
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz

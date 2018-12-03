Alberto97/android_device_motorola_addison

Add Camera shims

BoardConfig.mk
@@ -150,7 +150,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 # Shim
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib64/libmdmcutback.so|libqsap_shim.so \
-    /system/vendor/lib64/libsensorndkbridge.so|libshim_ril.so
+    /system/vendor/lib64/libsensorndkbridge.so|libshim_ril.so \
+    /system/vendor/lib/libmot_gpu_mapper.so|libgpu_mapper_shim.so \
+    /system/lib/libjustshoot.so|libjustshoot_shim.so
 
 # Sensors
 BOARD_USES_MOT_SENSOR_HUB := true

Sohamlad7/android_device_motorola_cedric

BoardConfig.mk
@@ -225,10 +225,9 @@ MOT_SENSOR_HUB_FEATURE_GR := true
 # Shims
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/bin/adspd|libshim_adsp.so \
-    /system/lib/lib_motsensorlistener.so|libsensor.so \
-    /system/lib/libjustshoot.so|libshims_camera.so \
-    /system/vendor/lib/libHui.so|libshim_camera_hal.so \
-    /system/vendor/lib64/libmdmcutback.so|libqsap_shim.so
+    /system/vendor/lib64/libmdmcutback.so|libqsap_shim.so \
+    /system/vendor/lib/libmot_gpu_mapper.so|libgpu_mapper_shim.so \
+    /system/lib/libjustshoot.so|libjustshoot_shim.so
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
72f37163dbb773973f98aa50a046c0600fa9c69c
Brandon Solis <bsolis@codeaurora.org>
2018-12-28 16:06:15 -0800

    cedric: Update user for ims daemons
    Change user for imsdatadaemon, imsrcsd,
    ims_rtp_daemon
    from AID_SYSTEM to AID_RADIO
    
    Change-Id: I8ee1c4ff182af0d9b4ebf028e3096cfa88118287
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.fs
+++ config.fs
@@ -26,13 +26,13 @@ group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND
 
 [system/vendor/bin/imsdatadaemon]
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 mode: 0755
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/ims_rtp_daemon]
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 mode: 0755
 caps: NET_BIND_SERVICE

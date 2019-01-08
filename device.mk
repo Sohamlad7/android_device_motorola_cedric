302dff4de39d88c51fc17cfa1d1e6feef87ba28a
Vachounet <vachounet@live.fr>
2019-01-08 13:57:12 +0100

    cedric: build android.hidl.manager-V1.0-java

--- device.mk
+++ device.mk
@@ -149,7 +149,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    android.hidl.manager-V1.0-java
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \

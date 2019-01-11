e8df0ee4c7939c99de82ecf7c65b5fb06ddc0246
Sohamlad7 <sohamlad7@gmail.com>
2019-01-11 13:19:26 +0530

    cedric: add stock Oreo fingerprint

--- init/init_cedric.cpp
+++ init/init_cedric.cpp
@@ -97,6 +97,10 @@ void vendor_load_properties()
     std::string sku = android::base::GetProperty("ro.boot.hardware.sku", "");
     property_override_dual("ro.product.model", "ro.vendor.product.model", sku.c_str());
 
+    // fingerprint
+    property_override("ro.build.description", "cedric-user 8.1.0 OPPS28.85-13-2 d04a4 release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "motorola/cedric/cedric:8.1.0/OPPS28.85-13-2/d04a4:user/release-keys");
+
     // rmt_storage
     std::string device = android::base::GetProperty("ro.boot.device", "");
     std::string radio = android::base::GetProperty("ro.boot.radio", "");

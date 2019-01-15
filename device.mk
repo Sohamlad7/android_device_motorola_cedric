4768383a70c6576dcdc4a4a145803e3c3ed0388c
Bruno Martins <bgcngm@gmail.com>
2019-01-15 00:24:17 +0000

    cedric: Replace KeyDisabler class with Touch HAL
    Change-Id: I7b87ae0ad834ba02a78696afe393d9d4f8920fbd

--- device.mk
+++ device.mk
@@ -385,6 +385,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-cedric.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.cedric
+
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
     textclassifier.bundle1

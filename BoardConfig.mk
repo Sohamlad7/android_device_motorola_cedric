e1da505aad53dfd1741d135356e65c864fbf172c
Sohamlad7 <sohamlad7@gmail.com>
2019-01-20 17:36:36 +0530

    cedric: change rild.libpath and sbin path to vendor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,7 +133,7 @@ BOARD_HAVE_BLUETOOTH_QCOM := true
 
 # Camera
 TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
-    /system/vendor/bin/mm-qcamera-daemon=26
+    /vendor/bin/mm-qcamera-daemon=26
 USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_QTI_CAMERA_32BIT_ONLY := true
 

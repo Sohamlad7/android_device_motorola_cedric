7fb3dd70067c9763a2baec5399b8d2348d875240
Vachounet <vachounet@live.fr>
2019-01-23 16:18:19 +0100

    cedric: switch to binderized light service
    Change-Id: I74e06a2c112d9a447ab86c50c13fab69687b4454

--- device.mk
+++ device.mk
@@ -244,7 +244,7 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
+    android.hardware.light@2.0-service.cedric \
     lights.msm8937
 
 # Display Calibration

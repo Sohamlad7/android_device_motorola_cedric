9d4a7bb799a9012995e21a7aa6895c2ff8af977b
Sohamlad7 <sohamlad7@gmail.com>
2019-03-31 12:39:45 +0530

    cedric: Move fingerprint back to makefile

--- lineage_cedric.mk
+++ lineage_cedric.mk
@@ -33,3 +33,9 @@ PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="cedric-user 8.1.0 OPPS28.85-13-2 d04a4 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := motorola/cedric/cedric:8.1.0/OPPS28.85-13-2/d04a4:user/release-keys

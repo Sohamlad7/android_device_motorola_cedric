1f0b000e3c8f1ba8fc2468372ddb5b197a24f7c0
Artem Borisov <dedsa2002@gmail.com>
2019-04-04 20:00:11 +0530

    cedric: touch: Don't include <binder/ProcessState.h>
    * Breaks build with BOARD_VNDK_VERSION=current
    
    Change-Id: I0172be3ad1b8328679830d5d6910e920e12b1959

--- touch/service.cpp
+++ touch/service.cpp
@@ -17,7 +17,6 @@
 #define LOG_TAG "vendor.lineage.touch@1.0-service.cedric"
 
 #include <android-base/logging.h>
-#include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
 
 #include "KeyDisabler.h"

634b535c0c58b702e4358f8c37e86ca23d08aa86
Sohamlad7 <sohamlad7@gmail.com>
2019-04-06 19:44:18 +0530

    cedric: switch back to Camera2
    * Camera2 is more reliable than snap for now

--- device.mk
+++ device.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    Snap \
+    Camera2 \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \

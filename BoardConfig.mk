cdedcd5a81870d4978b388d7b5f764d3ab1fe167
Sohamlad7 <sohamlad7@gmail.com>
2019-06-10 18:19:22 +0530

    Revert "cedric: Mark camera-related processes as API level 26."
    This reverts commit 91d42b080bac1a14c72a963b9acc6fffdad17a57.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,8 +129,6 @@ BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
 
 # Camera
-TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
-    /system/vendor/bin/mm-qcamera-daemon=26
 USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_QTI_CAMERA_32BIT_ONLY := true
 

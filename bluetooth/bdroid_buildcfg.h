a8ba5b316b47f04abfec1fee7d4a8581813b19be
LuK1337 <priv.luk@gmail.com>
2019-09-15 00:45:36 +0200

    cedric: bluetooth: Push×pop PROPERTY_VALUE_MAX in buildcfg header
    This deals with the PROPERTY_VALUE_MAX redefinition compilation issue.
    
    Change-Id: I34bb283512ca1a75c680af140846f16cccf0c645

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,6 +17,8 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
+#pragma push_macro("PROPERTY_VALUE_MAX")
+
 #include <cutils/properties.h>
 #include <string.h>
 
@@ -29,5 +31,5 @@
 #define BTM_WBS_INCLUDED TRUE
 #define BTIF_HF_WBS_PREFERRED TRUE
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
-#undef PROPERTY_VALUE_MAX
+#pragma pop_macro("PROPERTY_VALUE_MAX")
 #endif

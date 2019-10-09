cf4592b7572780b52124bc361942866f38463ecb
Sohamlad7 <sohamlad7@gmail.com>
2019-10-09 12:43:57 +0530

    cedric: enable DEXPREOPT

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,14 +55,8 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_OTA_ASSERT_DEVICE := cedric,cedric_retail
 
 # Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+WITH_DEXPREOPT := true
 
 # GPS
 TARGET_NO_RPC := true

d45dff7ed5eebade1e4033afd18d711f8e9fc9f2
kubersharma001 <kubersharma001@gmail.com>
2019-10-19 20:22:09 +0200

    cedric: Move powerhint.xml to vendor
    Change-Id: I74c9b888e435ed95a64c5218cc1986b215f2f7aa

--- device.mk
+++ device.mk
@@ -341,7 +341,7 @@ PRODUCT_PACKAGES += \
 
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml \
+     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
      $(LOCAL_PATH)/configs/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml
 
 # RIL

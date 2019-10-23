bebc904b61a5d386ffa6d76b3e8d820c82916b40
kubersharma001 <kubersharma001@gmail.com>
2019-10-23 19:49:09 +0530

    cedric: Drop deprecated WLAN SDK flag
    Change-Id: I8b96e42e5ca302720e2e1a88fb7e665132feb2ee

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -249,7 +249,6 @@ PRODUCT_VENDOR_MOVE_ENABLED      := true
 
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
-BOARD_HAS_QCOM_WLAN_SDK          := true
 BOARD_WLAN_DEVICE                := qcwcn
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn

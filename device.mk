549fd2155d6b192fad6c6d1f52324c157f3dff06
Erfan Abdi <erfangplus@gmail.com>
2019-10-28 12:23:46 +0530

    cedric: Build AOSP camera provider service

--- device.mk
+++ device.mk
@@ -111,6 +111,7 @@ PRODUCT_PACKAGES += \
     Camera2 \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \

f84f5beebb6a96248241f4afc6ff55c28072d408
Sohamlad7 <sohamlad7@gmail.com>
2019-11-01 19:17:26 +0530

    cedric: thermal-engine-cedric.conf -> thermal-engine.conf

--- device.mk
+++ device.mk
@@ -396,7 +396,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-cedric.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # Touch HAL
 PRODUCT_PACKAGES += \

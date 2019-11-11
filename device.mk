614b9eab7ee62dc8c8d230ff4188cab0bb2bb578
Sohamlad7 <sohamlad7@gmail.com>
2019-11-11 18:56:22 +0530

    cedric: switch to power@1.2 HIDL

--- device.mk
+++ device.mk
@@ -308,7 +308,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Public Libraries
 PRODUCT_COPY_FILES += \

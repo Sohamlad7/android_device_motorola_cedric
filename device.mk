4df806e64b13b91a1df9bf548f961fccf08dd4f9
kubersharma001 <kubersharma001@gmail.com>
2019-11-21 03:10:19 +0100

    Revert "cedric: Add -qti suffix to power@1.2 HIDL"

--- device.mk
+++ device.mk
@@ -307,7 +307,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.2-service
 
 # Public Libraries
 PRODUCT_COPY_FILES += \

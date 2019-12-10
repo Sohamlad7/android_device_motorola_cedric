86d5f0c8a26978d45e95dde7a3df94aa950ce921
kubersharma001 <kubersharma001@gmail.com>
2019-12-10 10:23:32 +0530

    Revert "cedric: Turn on GPU protected composition."
    Change-Id: I5aaebad2331fb3c72f35bd4ae167c63269fb05fd

--- device_props.mk
+++ device_props.mk
@@ -329,6 +329,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
